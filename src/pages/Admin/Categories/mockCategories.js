d878e45d5160720bf06f14fdef96268409ff8e29
lnminh99s <lnminh99s@gmail.com>
2025-04-25 09:10:56 +0700

    update login

---
+++ src/pages/Admin/Categories/mockCategories.js
@@ -0,0 +1,23 @@
+const mockCategories = [
+    {
+      id: 1,
+      categoryName: "Trà Sữa",
+      description: "Các loại trà sữa truyền thống và đặc biệt",
+      status: true,
+    },
+    {
+      id: 2,
+      categoryName: "Topping",
+      description: "Topping đi kèm cho đồ uống",
+      status: true,
+    },
+    {
+      id: 3,
+      categoryName: "Combo Ưu Đãi",
+      description: "Gói combo hấp dẫn tiết kiệm hơn",
+      status: false,
+    },
+  ];
+  
+  export default mockCategories;
+  
